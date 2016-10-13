042e5cd65874e02d5dc3d3f66290c2d4cb4a867a
Yudai Udagawa <nimius.rd@gmail.com>
2016-10-14 01:02:08 +0900

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -23,8 +23,7 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 # build CI
@@ -35,10 +34,10 @@ git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# if [ -z `git diff --exit-code` ]; then
+#     echo "No changes to the output on this push; exiting."
+#     exit 0
+# fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
