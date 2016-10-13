73ea5945ae21adc817fbaa19b3d8de6719d36c8d
Yudai Udagawa <nimius.rd@gmail.com>
2016-10-14 00:22:42 +0900

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -25,16 +25,14 @@ SHA=`git rev-parse --verify HEAD`
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf . || exit 0
 
 # build CI
 buildci
 
 # Now let's go have some fun with the cloned repo
-cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
