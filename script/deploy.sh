8b9cd19d1f0a88fca1470f34c1391a697c9c083b
Yudai Udagawa <nimius.rd@gmail.com>
2016-10-14 01:23:50 +0900

    fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -23,11 +23,13 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git clone $REPO out
+cd out
 
 # build CI
 buildci
+
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
