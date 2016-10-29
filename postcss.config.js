2e2ff49d5abc89f526ce222d542a930b557288a5
Yudai Udagawa <nimius.rd@gmail.com>
2016-10-29 21:46:49 +0900

    add postcss packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,45 @@
-/*eslint no-process-env: 0 */
+/* eslint no-process-env: 0 */
+/* eslint sort-keys: 0 */
+
+const cssnanoOption = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return false;
+  }
+  const option = {
+    'autoprefixer': {
+      add: true,
+      browsers: [
+        /* ref: http://browserl.ist/?q=last+2+versions */
+        'last 2 versions',
+        '> 1%'
+      ],
+    },
+    'normalizeCharset': {
+      add: true
+    }
+  };
+
+  return option;
+};
 
 module.exports = {
   map: false,
   plugins: {
-    'cssnano': {
-      'autoprefixer': {
-        add: true,
-        browsers: [
-          /* ref: http://browserl.ist/?q=last+2+versions */
-          'last 2 versions',
-          '> 1%'
-        ],
-      },
-      'core': process.env.NODE_ENV === 'production',
-      'normalizeCharset': {
-        add: true
-      }
-    },
     'postcss-apply': {},
     'postcss-custom-media': {},
     'postcss-custom-properties': {},
+    'postcss-font-smoothing': {},
+    'postcss-initial': {},
     'postcss-media-minmax': {},
     'postcss-nesting': {},
+    // NOTE: Don't change the order below.
+    'postcss-autoreset': {
+      reset: {
+        padding: 0,
+        margin: 0,
+        borderRadius: 0
+      }
+    },
+    'cssnano': cssnanoOption()
   }
 };
