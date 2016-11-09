c26ba4d4c63fe3acd9e57bee4e12e6b3691ba6bb
Yudai Udagawa <nimius.rd@gmail.com>
2016-11-09 23:46:52 +0900

    fix animation

--- webpack/js/drawerToggle.js
+++ webpack/js/drawerToggle.js
@@ -3,22 +3,12 @@ const drawer = document.getElementById('drawer');
 const toggle = document.getElementById('toggle');
 const obfucator = document.getElementById('obfucator');
 
-const toggleAnimetion = function handleClass(elem, elemId = elem.id) {
-  if (elem.classList.contains(`${elemId}--animation__show`)) {
-    elem.classList.toggle(`${elemId}--animation__hide`);
-  } else {
-    elem.classList.toggle(`${elemId}--animation__show`);
-  }
-};
-
 toggle.addEventListener('click', () => {
   drawer.classList.toggle('drawer__visible');
-  toggleAnimetion(drawer);
   obfucator.classList.toggle('obfucator__visible');
 });
 
 obfucator.addEventListener('click', () => {
   drawer.classList.toggle('drawer__visible');
-  toggleAnimetion(drawer);
   obfucator.classList.toggle('obfucator__visible');
 });
