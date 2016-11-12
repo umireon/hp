5276c50e2bd8430410e61379804b57eca4c3e014
Yudai Udagawa <nimius.rd@gmail.com>
2016-11-12 19:40:44 +0900

    remove unused files

--- tools/server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-rm -vrf _site
-
-yarn run build
-bundle exec jekyll serve
--- tools/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-PROJECT_DIR=`git rev-parse --show-toplevel`
-
-$PROJECT_DIR/tools/build.sh
-bundle exec htmlproofer ./_site
